fa263240b790e16d027d73736f9e80daeb2220a2
fathimashaik <145197132+Fathimasha@users.noreply.github.com>
2026-08-17 11:58:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Factorial/factorial.java
@@ -0,0 +1,11 @@
+class Solution {
+    static int factorial(int n) {
+        // code here
+        int fac=1;
+        for(int i=1;i<=n;i++)
+        {
+            fac=fac*i;
+        }
+        return fac;
+    }
+}
